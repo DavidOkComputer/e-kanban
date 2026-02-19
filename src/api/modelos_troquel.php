9a2d6f0775bd405788bec2852d5c8ae79a597fd0
DavidOkComputer <francisco.barreto@uttn.mx>
2026-02-19 15:23:22 -0600

    update of 29/02/26 at 15:23

--- src/api/modelos_troquel.php
+++ src/api/modelos_troquel.php
@@ -1,4 +1,6 @@
 <?php
+//modelos_troquel.php para obtener los distintos modelos de los troqueles
+
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
